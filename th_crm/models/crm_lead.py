dc920e13320f05e4f08c9af299b378866693f0f1
TugayHatil <tugay.hatill@gmail.com>
2026-08-12 01:37:57 +0300

    [REMOVE] Remove pipeline_id field from crm.lead model

--- th_crm/models/crm_lead.py
+++ th_crm/models/crm_lead.py
@@ -7,7 +7,6 @@ class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
     dr_no = fields.Char(string='DR No')
-    pipeline_id = fields.Many2one('crm.pipeline', string='Pipeline')
 
     # Qualification / Opportunity
     sequence_enrollment = fields.Boolean(string='Sequence enrollment')
