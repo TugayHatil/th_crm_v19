170db9169472442b42c3964607e4931450aaf29f
TugayHatil <tugay.hatill@gmail.com>
2026-08-11 23:54:36 +0300

    [MOD] Move DR No to qualification section, add RISK Html field

--- th_crm/models/crm_lead.py
+++ th_crm/models/crm_lead.py
@@ -48,5 +48,5 @@ class CrmLead(models.Model):
     authority = fields.Char(string='AUTHORITY:Who is Executive Sponsor? Who Own Funds?Who Sign Contract?')
     budget = fields.Char(string='BUDGET:Who owns the Budget? Is it funded for Outside Purchase?')
     lead_source_details = fields.Html(string='Details for Lead Source')
-    # risk = fields.Html(string='RISK:General Risks? Product Risks:')
+    risk = fields.Html(string='RISK:General Risks? Product Risks:')
     # timing = fields.Html(string='TIMING:Timeframe? Why Buy Now?')
