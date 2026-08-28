2694995d6eb0a56cffe29757cb64010fb9dd8a30
TugayHatil <tugay.hatill@gmail.com>
2026-08-28 11:45:18 +0300

    [FIX] Add action_id to ir.filters create for proper filter association

--- th_crm/models/informatica_pipeline.py
+++ th_crm/models/informatica_pipeline.py
@@ -16,10 +16,12 @@ class InformaticaPipeline(models.Model):
     def create(self, vals_list):
         pipelines = super().create(vals_list)
         crm_lead_model = self.env.ref('crm.model_crm_lead')
+        crm_lead_action = self.env.ref('crm.action_crm_lead')
         for pipeline in pipelines:
             self.env['ir.filters'].create({
                 'name': pipeline.name,
                 'model_id': crm_lead_model.id,
+                'action_id': crm_lead_action.id,
                 'domain': f"[('pipeline_id', '=', {pipeline.id})]",
                 'context': "{}",
                 'is_default': True,
