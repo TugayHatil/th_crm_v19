dd4f363210cdcef522c225f4c5d25b3975b9dbe9
TugayHatil <tugay.hatill@gmail.com>
2026-09-03 18:10:56 +0300

    [FIX] Override read_group method to filter stages by pipeline in kanban view

--- crm_komtas_ux/models/crm_lead.py
+++ crm_komtas_ux/models/crm_lead.py
@@ -39,19 +39,20 @@ class CrmLead(models.Model):
         domain="[('pipeline_id', '=', pipeline_id)]"
     )
 
-    @api.model
-    def _read_group_stage_ids(self, stages, domain, order):
-        pipeline_id = self._context.get('default_pipeline_id')
-        if not pipeline_id:
-            pipeline_ids = self.search(domain).mapped('pipeline_id').ids
-            if pipeline_ids and len(pipeline_ids) == 1:
-                pipeline_id = pipeline_ids[0]
-        if pipeline_id:
-            search_domain = ['|', ('id', 'in', stages.ids), '|', ('pipeline_id', '=', False), ('pipeline_id', '=', pipeline_id)]
-        else:
-            search_domain = ['|', ('id', 'in', stages.ids), ('pipeline_id', '=', False)]
-        stage_ids = stages._search(search_domain, order=order, access_rights_uid=self.env['ir.rule']._get_user_id())
-        return stages.browse(stage_ids)
+    def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
+        if groupby and 'stage_id' in groupby:
+            pipeline_id = self.env.context.get('default_pipeline_id')
+            if not pipeline_id:
+                pipeline_ids = self.search(domain).mapped('pipeline_id').ids
+                if pipeline_ids and len(pipeline_ids) == 1:
+                    pipeline_id = pipeline_ids[0]
+            if pipeline_id:
+                stage_domain = [('pipeline_id', '=', pipeline_id)]
+                stages = self.env['crm.stage'].search(stage_domain)
+                if stages:
+                    stage_ids = stages.ids
+                    domain = domain + [('stage_id', 'in', stage_ids)]
+        return super(CrmLead, self).read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
 
     service_competitor_ids = fields.Many2many(
         'res.partner',
