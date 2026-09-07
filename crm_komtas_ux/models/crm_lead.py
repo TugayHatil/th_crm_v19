b8d0b5f3464b2c555a8763b13748468ccdc5d519
TugayHatil <tugay.hatill@gmail.com>
2026-09-07 14:47:04 +0300

    Fix stage required fields validation: use constrains + sudo

--- crm_komtas_ux/models/crm_lead.py
+++ crm_komtas_ux/models/crm_lead.py
@@ -143,30 +143,26 @@ class CrmLead(models.Model):
             self.create_date or fields.Date.context_today(self),
         )
 
-    def write(self, vals):
+    @api.constrains('stage_id')
+    def _check_required_fields_for_stage(self):
         """Aşama değişiminde, yeni aşamada zorunlu tanımlı alanlar boşsa hata fırlat."""
-        if 'stage_id' in vals:
-            new_stage = self.env['crm.stage'].browse(vals['stage_id'])
-            if new_stage.required_fields:
-                missing_per_lead = []
-                for lead in self:
-                    missing_labels = []
-                    for field in new_stage.required_fields:
-                        fname = field.name
-                        # Değer vals içinde mi geliyor, yoksa kayıtta mevcut mu?
-                        value = vals.get(fname, getattr(lead, fname, False))
-                        # Many2one için id kontrolü
-                        if hasattr(value, 'id'):
-                            value = value.id
-                        if not value and value != 0:
-                            missing_labels.append(field.field_description)
-                    if missing_labels:
-                        missing_per_lead.append(
-                            "'%s' için zorunlu alanlar: %s" % (lead.name, ', '.join(missing_labels))
-                        )
-                if missing_per_lead:
-                    raise ValidationError(
-                        "'%s' aşamasına geçiş için aşağıdaki zorunlu alanları doldurun:\n\n%s"
-                        % (new_stage.name, '\n'.join(missing_per_lead))
-                    )
-        return super().write(vals)
+        for lead in self:
+            # sudo() ile ir.model.fields erişim kısıtını aşıyoruz
+            required = lead.stage_id.sudo().required_fields
+            if not required:
+                continue
+            missing_labels = []
+            for field in required:
+                fname = field.name
+                value = getattr(lead, fname, False)
+                # Many2one için recordset kontrolü
+                if hasattr(value, '_name'):
+                    value = value.id if value else False
+                if not value and value != 0:
+                    missing_labels.append(field.field_description)
+            if missing_labels:
+                raise ValidationError(
+                    "'%s' aşamasına geçiş için aşağıdaki zorunlu alanları doldurun:\n\n%s"
+                    % (lead.stage_id.name, ', '.join(missing_labels))
+                )
+
