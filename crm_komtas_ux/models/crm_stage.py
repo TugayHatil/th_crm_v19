6297c4c353115184d2c1b7d2c6a306d26bf97572
TugayHatil <tugay.hatill@gmail.com>
2026-09-08 10:38:05 +0300

    [FIX] Domain: allow many2many fields (store=False) in required_fields selection

--- crm_komtas_ux/models/crm_stage.py
+++ crm_komtas_ux/models/crm_stage.py
@@ -20,6 +20,10 @@ class CrmStage(models.Model):
         'stage_id',
         'field_id',
         string='Required Fields',
-        domain=[('model_id.model', '=', 'crm.lead'), ('ttype', 'not in', ['one2many', 'binary']), ('store', '=', True)],
+        domain=[
+            ('model_id.model', '=', 'crm.lead'),
+            '|', ('ttype', '=', 'many2many'),
+            '&', ('ttype', 'not in', ['one2many', 'binary']), ('store', '=', True),
+        ],
         help='Bu aşamaya geçildiğinde seçilen alanlar zorunlu hale gelir.',
     )
